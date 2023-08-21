a1174a9ed2eff3a6a7199e35b04447d443acf6b7
PisitTr65 <pisit.tr.65@ubu.ac.th>
2023-08-21 21:09:17 +0700

    midterm-repair

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import { useState } from "react";
+
+function App() {
+  const [num, setNum] = useState(0);
+  let isPrime = "";
+  if (num === 1) {
+    isPrime = `${num} เป็นทั้งจำนวนเฉพาะและ<strong>ไม่</strong>เป็นจำนวนเฉพาะ`;
+  } else if (num > 1) {
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        isPrime = `${num} เป็นจำนวนเฉพาะ`;
+      }
+    }
+    if (isPrime) {
+      isPrime = `${num}<strong> ไม่</strong>เป็นจำนวนเฉพาะ`;
+    } else {
+      isPrime = `${num} เป็นจำนวนเฉพาะ`;
+    }
+  }
+  return (
+    <div className="App">
+      <Header />
+      <div className="MyContent">
+        <h2>{num}</h2>
+        <button onClick={() => setNum(num + 1)} class="btn btn-primary">
+          เพิ่ม
+        </button>
+        &nbsp;
+        <button onClick={() => setNum(num - 1)} class="btn btn-secondary">
+          ลด
+        </button>
+        <br></br>
+        <span dangerouslySetInnerHTML={{ __html: isPrime }}></span>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
